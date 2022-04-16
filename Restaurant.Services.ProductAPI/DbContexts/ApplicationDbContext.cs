034f708975c6918e96349fdfca57dfba22db239f
Varun Rampal <varun.rampall@gmail.com>
2022-04-16 14:12:57 -0700

    Add project files.

---
+++ Restaurant.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Services.ProductAPI.Models;
+
+namespace Restaurant.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
