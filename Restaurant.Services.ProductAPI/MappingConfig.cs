4cedc0775cb6923d65cddf1ec11062625266f320
Varun Rampal <varun.rampall@gmail.com>
2022-05-11 12:28:10 -0700

    Product API setup

---
+++ Restaurant.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Restaurant.Services.ProductAPI.Models;
+using Restaurant.Services.ProductAPI.Models.Dtos;
+
+namespace Restaurant.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
--- Restaurant.Services.ProductAPI/Repository/IProductRepository.cs
+++ Restaurant.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Restaurant.Services.ProductAPI.Repository
+﻿using Restaurant.Services.ProductAPI.Models.Dtos;
+
+namespace Restaurant.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
